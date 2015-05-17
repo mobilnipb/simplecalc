caef6d9894aac6763051ade44d7f301bd5cdf713
mobilnipb <mobilnipb@gmail.com>
2015-05-18 00:23:48 +0200

    First Version with button and toast Pierwsza wersja z przyciskiem i Komunikatem

--- src/com/example/simplecalc/MainActivity.java
+++ src/com/example/simplecalc/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplecalc;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 import android.app.Activity;
 
 
@@ -12,6 +15,10 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-
+	public void  Podaj_wynik(View v)
+	{
+		Log.w("simplecalc", "kliknieto");
+		Toast.makeText(getApplicationContext(), "kliknales mnie", Toast.LENGTH_SHORT).show();
+	}
 
 }
