a4d939eb1240edaa8444ac8681f9828201dd4039
mobilnipb <mobilnipb@gmail.com>
2015-05-18 00:50:18 +0200

    Made calculation after click
    Wykonane obliczenie po przyciśnięciu

--- src/com/example/simplecalc/MainActivity.java
+++ src/com/example/simplecalc/MainActivity.java
@@ -3,22 +3,33 @@ package com.example.simplecalc;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 
 
 public class MainActivity extends Activity {
-
+	private EditText num; 
+	private TextView wyn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		 num = (EditText) findViewById(R.id.L2) ;
+		 wyn = (TextView) findViewById(R.id.txWynik);
+		
 	}
 
 	public void  Podaj_wynik(View v)
 	{
 		Log.w("simplecalc", "kliknieto");
-		Toast.makeText(getApplicationContext(), "kliknales mnie", Toast.LENGTH_SHORT).show();
+		//Toast.makeText(getApplicationContext(), "kliknales mnie", Toast.LENGTH_SHORT).show();
+		int val = Integer.parseInt( num.getText().toString() );
+		val=val+5;
+		wyn.setText(String.valueOf(val));
+		
+		
 	}
 
 }
